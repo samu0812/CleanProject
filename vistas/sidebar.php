af41f200c991f8fd2905808275938c188f7de8a5
CeciGaspar <cecigaspar15@icloud.com>
2023-08-23 19:16:16 -0300

    ultimos cambios ceci

--- vistas/sidebar.php
+++ vistas/sidebar.php
@@ -23,13 +23,14 @@ $rol = isset($_SESSION['rol']) ? $_SESSION['rol'] : '';
             </div>
         </div>
         <div class="navbar-nav w-100">
-            <a href="home.php" class="nav-item nav-link active mb-2"><i class="fa fa-home me-2"></i>Home</a>
-            <a href="productos.php" class="nav-item nav-link active mb-2"><i class="fas fa-warehouse me-2"></i>Productos</a>
-            <a href="proveedores.php" class="nav-item nav-link active mb-2"><i class="fa fa-truck me-2"></i>Proveedores</a>
-            <a href="ventas.php" class="nav-item nav-link active mb-2"><i class="fa fa-shopping-bag me-2"></i>Ventas</a>
-            <a href="facturacion.php" class="nav-item nav-link active mb-2"><i class="fab fa-elementor me-2"></i>Facturación</a>
-            <a href="reportes.php" class="nav-item nav-link active mb-2"><i class="fas fa-paste me-2"></i>Reportes</a>
-            <a href="usuarios.php" class="nav-item nav-link active mb-2"><i class="fa fa-users me-2"></i>Admin Usuarios</a>
+            <a href="home.php" class="nav-item nav-link active mb-1"><i class="fa fa-home me-2"></i>Home</a>
+            <a href="productos.php" class="nav-item nav-link active mb-1"><i class="fas fa-warehouse me-2"></i>Productos</a>
+            <a href="proveedores.php" class="nav-item nav-link active mb-1"><i class="fa fa-truck me-2"></i>Proveedores</a>
+            <a href="usuarios.php" class="nav-item nav-link active mb-1"><i class="fa fa-users me-2"></i>Pedidos</a>
+            <a href="ventas.php" class="nav-item nav-link active mb-1"><i class="fa fa-shopping-bag me-2"></i>Ventas</a>
+            <a href="facturacion.php" class="nav-item nav-link active mb-1"><i class="fab fa-elementor me-2"></i>Facturación</a>
+            <a href="reportes.php" class="nav-item nav-link active mb-1"><i class="fas fa-paste me-2"></i>Reportes</a>
+            <a href="usuarios.php" class="nav-item nav-link active mb-1"><i class="fa fa-users me-2"></i>Admin Usuarios</a>
 
         </div>
         
